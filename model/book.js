3d4c503101759431e74c826b63f359a04cd08b27
Moetive <Moetive206@gmail.com>
2022-07-12 20:23:55 -0700

    books.js and index.js updated
    added books model and index.js code

---
+++ model/book.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Books extends Model {}
+
+Books.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+          title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+          ISBN: {
+            type: DataTypes.INTEGER,
+          },
+          stock: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+          },
+          category: {
+            type: DataTypes.STRING,
+            references: {
+                model: 'category',
+                key: 'id',
+              },
+          },
+          comments: {
+            type: DataTypes.STRING,
+          },   
+    }
+)

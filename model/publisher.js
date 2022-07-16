8eb69a83e40c4a4c169a222a86ec7c34b41e7073
Moetive <Moetive206@gmail.com>
2022-07-16 10:34:46 -0700

    bookData.json & publisher.json
    created bookData.Json & removed publisher.js and updated book.js & seed.js

--- model/publisher.js
+++ model/publisher.js
@@ -1,32 +1,32 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection.js');
-const book = require('../model/book.js')
-const category = require('../model/category.js')
+// const { Model, DataTypes } = require('sequelize');
+// const sequelize = require('../config/connection.js');
+// const book = require('../model/book.js')
+// const category = require('../model/category.js')
 
-class publisher extends Model {}
+// class publisher extends Model {}
 
 
 
-publisher.init(
-  {
-    // define columns
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    publisher_name: {
-      type: DataTypes.STRING,
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'publisher',
-  }
-);
+// publisher.init(
+//   {
+//     // define columns
+//     id: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       primaryKey: true,
+//       autoIncrement: true
+//     },
+//     publisher_name: {
+//       type: DataTypes.STRING,
+//     },
+//   },
+//   {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: 'publisher',
+//   }
+// );
 
-module.exports = publisher;
+// module.exports = publisher;

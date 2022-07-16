a71e2dfb3cc474a4b51549242428cec580f7708d
Chris Faust <hello@chrisfaust.dev>
2022-07-16 11:23:27 -0700

    added server.js file and some starter code to controllers file

---
+++ controllers/api/indexAPI.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+router.use('/users', userRoutes);
+router.use('', );
+router.use('', );
+
+module.exports = router;
---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const { User } = require('../../model');
+
+
+
+
+
+module.exports = router;

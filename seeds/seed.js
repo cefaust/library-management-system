16e9c2cab7a2b5047deb422de41fbfe3c0ee5d81
Chris Faust <hello@chrisfaust.dev>
2022-07-16 11:40:53 -0700

    made changes to the seeds.js file deleted the nindex.js from seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,16 +1,21 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Books Categories } = require('../models');
 
 const userData = require('./userData.json');
 const bookData = require('./bookData.json');
 const categoriesData = require('./categoriesData.json');
+
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await User.bulkCreate(userData, bookData, categoriesData, {
+  await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
+
+  await  Books.bulkCreate(bookData)
+
+  await Categories.bulkCreate(categoriesData);
 
   process.exit(0);
 };

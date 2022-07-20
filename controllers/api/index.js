4b90eac22b2d68b9eb5753f0f5a496bf81c76581
Chris Faust <hello@chrisfaust.dev>
2022-07-19 20:54:32 -0700

    fixed single categories

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const categoriesRoutes = require('./categoriesRoutes');
 const userRoutes = require('./userRoutes');
 // const checkoutRoutes = require('./checkoutRoutes');
+const profileRoutes = require('./profileRoutes');
 
 router.use('/categories', categoriesRoutes);
 router.use('/users', userRoutes);
+router.use('/profile', profileRoutes);
 // router.use('/book', bookRoutes);
 // router.use('/checkout', checkoutRoutes);
 

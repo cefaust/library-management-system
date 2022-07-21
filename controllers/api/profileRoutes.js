843650926a78821e5602cad8fbbd1bd7176f8e1c
Chris Faust <hello@chrisfaust.dev>
2022-07-20 21:01:34 -0700

    changes to multiple files

--- controllers/api/profileRoutes.js
+++ controllers/api/profileRoutes.js
@@ -1,4 +1,39 @@
 const router = require('express').Router();
+const { Book , User, Checkout } = require('../../model')
+const withAuth = require('../../utils/auth');
 
+router.post('/', withAuth, async (req, res) => {
+  try {
+
+    const newCheckoutBook = await Checkout.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newCheckoutBook);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const checkedOutBookData = Checkout.destroy( {
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!checkedOutBookData) {
+      res.status(404).json({message: 'No project found with this id'});
+      return;
+    }
+
+    res.status(2300).json(checkedOutBookData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
 
 module.exports = router;
